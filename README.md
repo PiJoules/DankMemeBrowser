76ac875a033290284b6541991aa2a1c93c65d4e7
pijoules <lchan1994@yahoo.com>
2015-09-03 18:37:02 -0400

    because I cannot actually get the image URL from google, I will instead browse reddit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dank Meme Search Engine
-Search engine for specifically browsing dank memes.
+Search engine for specifically browsing dank memes. (Essentially just reddit but for specifically dank memes.)
+
+## Dependencies
+- Bootstrap 3.3.5
+- jQuery
--- __init__.py
+++ __init__.py
@@ -8,16 +8,52 @@ vendor.add('lib')
 from flask import Flask, render_template, jsonify, request, url_for
 app = Flask(__name__)
 
-# Import google python module
-from google import search
+from urllib import quote_plus
+from urllib2 import Request, urlopen
+from bs4 import BeautifulSoup
 
 @app.route("/")
 def index_route():
 	q = request.args.get("q")
 	if q:
-		return jsonify(result=list(search(q,stop=10)))
+		return jsonify(tags=list(search_reddit_img(q)))
 	else:
 		return render_template("index.html")
+
+def get_page(url):
+	request = Request(url)
+	request.add_header('User-Agent','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)')
+	response = urlopen(request)
+	html = response.read()
+	response.close()
+	return html
+
+def search_reddit_img(q):
+	# Set of hashes for the results found.
+	# This is used to avoid repeated results.
+	hashes = set()
+
+	html = get_page("https://www.reddit.com/r/dankmemes/search?q=" + quote_plus(q) + "&restrict_sr=on&sort=relevance&t=all")
+	soup = BeautifulSoup(html, "html.parser")
+	anchors = soup.findAll('a')
+	for a in anchors:
+		# Get the URL from the anchor tag.
+		try:
+		    link = a['href']
+		except KeyError:
+		    continue
+
+		# Only accept imgur links for now
+		if not link.startswith("http://i.imgur.com/"):
+			continue
+
+		# Discard repeated results.
+		h = hash(link)
+		if h in hashes:
+		    continue
+		hashes.add(h)
+
+		yield link
 
 if __name__ == '__main__':
 	app.run()
